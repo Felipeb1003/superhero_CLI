d23a7ab71089b0071242c7d950a9d59bb355e44a
felipe <felipebeltran.nyc@gmail.com>
2020-05-26 17:00:44 -0400

    delet files

--- lib/superhero_CLI.rb
+++
@@ -1,6 +0,0 @@
-require "superhero_CLI/version"
-
-module SuperheroCLI
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/superhero_CLI/version.rb
+++
@@ -1,3 +0,0 @@
-module SuperheroCLI
-  VERSION = "0.1.0"
-end
