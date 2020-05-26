1901d738115f9c2b596194c0c739d96dbe3cc944
felipe <felipebeltran.nyc@gmail.com>
2020-05-26 16:42:07 -0400

    first commit

---
+++ lib/superhero_CLI.rb
@@ -0,0 +1,6 @@
+require "superhero_CLI/version"
+
+module SuperheroCLI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/superhero_CLI/version.rb
@@ -0,0 +1,3 @@
+module SuperheroCLI
+  VERSION = "0.1.0"
+end
