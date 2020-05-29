a8cd92c30c187d4569c85da5c1842c7392d90ac7
felipe <felipebeltran.nyc@gmail.com>
2020-05-29 17:42:13 -0400

    Add Colors

--- lib/superhero_CLI/CLI.rb
+++ lib/superhero_CLI/CLI.rb
@@ -2,8 +2,8 @@ class CLI
 
     def start
       puts "\n"
-      puts Rainbow("\nWelcome to the Superhero Fact Checker!").red
-      puts "\nHere you can find everything you want to know about your favorite SuperHeroes!"
+      puts Rainbow("\nWelcome to the Superhero Fact Checker!").red.underline
+      puts Rainbow("\nHere you can find everything you want to know about your favorite SuperHeroes!").darkgoldenrod
       self.first_step
     end
   
@@ -12,34 +12,32 @@ class CLI
           self.select_a_superhero
           self.display_categories
           self.select_a_category
-          sleep(1)
+          sleep(2)
           self.users_choice
    end
 
     def display_search_name_result
+        puts "\n"
         SuperHero.all.each.with_index(1) do |hero , index|
-        puts "#{index}. #{hero.name}."
+        puts Rainbow("#{index}. ").darkgoldenrod + "#{hero.name}."
         end
     end
 
     def display_categories
       @categories = ["Appearance", "Biography", "Powerstats", "Work"]
-      puts "Categories:"
+      puts Rainbow("Categories:").red
       @categories.each.with_index(1) do |category, index|
         
-        puts "#{index}. #{category}."
+        puts Rainbow("#{index}. ").darkgoldenrod + "#{category}." 
       end
     end
 
     def ask_user_for_superhero_name
-        puts "\n"
-        puts "\n"
-        puts "\n"
-        puts "Please enter the name of the Superhero you are looking for:"
+        puts Rainbow("\nPlease enter the name of the Superhero you are looking for:").cyan
         input_superhero_name= gets.strip.downcase
     
         until API.fetch_superhero(input_superhero_name) 
-            puts "Superhero not found. (ง'̀-'́)ง Please try again."
+            puts Rainbow("\nSuperhero not found.").cyan + Rainbow(" (ง'̀-'́)ง ").darkgoldenrod + Rainbow("Please try again.").cyan
             
             input_superhero_name= gets.strip.downcase
         end
@@ -48,27 +46,27 @@ class CLI
     end
     
     def select_a_superhero
-      puts "\nPlease Select the character you would like to look at:"
+      puts Rainbow("\nPlease Select the character you would like to look at:").cyan
        index = gets.strip.to_i - 1
        max_limit = SuperHero.all.length - 1
 
        until index.between?(0, max_limit) 
-        puts "⇪ Invalid number. Please try again."
+        puts Rainbow("\n⇪ ").darkgoldenrod +  Rainbow("Invalid number. Please try again.").cyan
         index = gets.strip.to_i - 1
        end
 
         @superhero_instance= SuperHero.all[index]
-        puts "\n *#{@superhero_instance.name}*"
+        puts Rainbow("*#{@superhero_instance.name}*").darkgoldenrod
        
     end
        
     def select_a_category
-      puts "Please select the category that you would like to see or type '4' to exit:"
+      puts Rainbow("\nPlease select the category that you would like to see or type '").cyan + Rainbow("5").darkgoldenrod + Rainbow("' to exit:").cyan
       index = gets.strip.to_i - 1
       
 
       until index.between?(0, 4)
-        puts "⇪ Invalid number. Please try again."
+        puts Rainbow("\n⇪ ").darkgoldenrod +  Rainbow("Invalid number. Please try again.").cyan
         index = gets.strip.to_i - 1 
       end  
 
@@ -90,55 +88,53 @@ class CLI
    end
    
    def user_chose_appearance
-    puts "\n*#{@superhero_instance.name}*"
-    puts "1. Appearance."
-    puts "\n"
+    puts Rainbow("\n *#{@superhero_instance.name}*").darkgoldenrod
+    puts Rainbow("   Appearance.").red
       
       @superhero_instance.appearance.each do |key, value|
         if key == "height" || key == "weight"
-          puts "-#{key.capitalize} = #{value[0]} || #{value[1]} "
+          puts Rainbow("    -").darkgoldenrod + Rainbow("#{key.capitalize}").cyan + Rainbow(" = ").darkgoldenrod + "#{value[0]}" + Rainbow(" || ").darkgoldenrod + "#{value[1]}"
         elsif
-          puts "-#{key.capitalize} = #{value}"
+          puts Rainbow("    -").darkgoldenrod + Rainbow("#{key.capitalize}").cyan + Rainbow(" = ").darkgoldenrod + "#{value}"
         end
       end
 
    end
 
    def user_chose_biography
-    puts "\n*#{@superhero_instance.name}*"
-    puts "2. Biography."
-    puts "\n"
+    puts Rainbow("\n *#{@superhero_instance.name}*").darkgoldenrod
+    puts Rainbow("   Biography.").red
     
       @superhero_instance.biography.each do |key, value|
         if key == "aliases"
           if value.length > 1  
-              puts "-#{key.capitalize} =" 
-              value.each{ |name| puts"~~>#{name}."}
+              puts Rainbow("    -").darkgoldenrod + Rainbow("#{key.capitalize}").cyan + Rainbow(" = ").darkgoldenrod
+              value.each{ |name| puts Rainbow("      ~>").darkgoldenrod + "#{name}."}
           elsif value.length == 1
-              puts "-#{key.capitalize} = #{value.join}"
+              puts Rainbow("    -").darkgoldenrod + Rainbow("#{key.capitalize}").cyan + Rainbow(" = ").darkgoldenrod + "#{value.join}"
           end
 
         elsif
-          puts "-#{key.capitalize} = #{value}"
+          puts Rainbow("    -").darkgoldenrod + Rainbow("#{key.capitalize}").cyan + Rainbow(" = ").darkgoldenrod + "#{value}"
         end 
 
       end
    end
 
    def user_chose_powerstats
-      puts "*#{@superhero_instance.name}*"
-      puts "3. Powerstats."
+      puts Rainbow("\n *#{@superhero_instance.name}*").darkgoldenrod
+      puts Rainbow("   Powerstats.").red
         @superhero_instance.powerstats.each do |key, value|
-          puts "-#{key.capitalize} = #{value}"
+          puts Rainbow("    -").darkgoldenrod + Rainbow("#{key.capitalize}").cyan + Rainbow(" = ").darkgoldenrod + "#{value}"
         end
 
    end
 
    def user_chose_work
-      puts "*#{@superhero_instance.name}*"
-      puts "4. Work."
+      puts Rainbow("\n *#{@superhero_instance.name}*").darkgoldenrod
+      puts Rainbow("   Work.").red
         @superhero_instance.work.each do |key, value|
-          puts "-#{key.capitalize} = #{value}"
+          puts Rainbow("    -").darkgoldenrod + Rainbow("#{key.capitalize}").cyan + Rainbow(" = ").darkgoldenrod + "#{value}"
         end
 
    end
@@ -146,29 +142,29 @@ class CLI
    def exit_program
       sleep(1)
       puts "\n"
-      puts ' "Dreams save us.'
-      puts "  Dreams lift us up and transform us."
-      puts "  And on my soul I swear..."
-      puts "  Until my dream of a world where dignity, honor,"
-      puts "  and justice becomes the reality we all share..."
-      puts "  I'll never stop fighting. EVER." + '"'
-      puts "  SUPERMAN"
+      puts Rainbow(' "').darkgoldenrod + Rainbow('Dreams save us.').cyan 
+      puts Rainbow("  Dreams lift us up and transform us.").cyan
+      puts Rainbow("  And on my soul I swear...").cyan 
+      puts Rainbow("  Until my dream of a world where dignity, honor,").cyan 
+      puts Rainbow("  and justice becomes the reality we all share...").cyan 
+      puts Rainbow("  I'll never stop fighting. EVER.").cyan + Rainbow('"').darkgoldenrod
+      puts "  " + Rainbow("SUPERMAN").red.underline
 
-      sleep(1)
+      sleep(3)
       exit  
    end
 
    def users_choice
-    puts "\n MENU"
-    puts "\nTo select another category: type '1'"
-    puts "To select a new superhero from your search: type '2'"
-    puts "To start a new search: type '3'"
-    puts "To exit: type '4'"
+    puts Rainbow("\n MENU").red
+    puts Rainbow("\nTo select another category: type '").cyan + Rainbow("1").darkgoldenrod + Rainbow("'").cyan
+    puts Rainbow("To select a new superhero from your search: type '").cyan + Rainbow("2").darkgoldenrod + Rainbow("'").cyan
+    puts Rainbow("To start a new search: type '").cyan + Rainbow("3").darkgoldenrod + Rainbow("'").cyan
+    puts Rainbow("To exit: type '").cyan + Rainbow("4").darkgoldenrod + Rainbow("'").cyan
     
     index = gets.strip.to_i
 
       until index.between?(1, 4)
-        puts "⇪ Invalid number. Please try again."
+        puts Rainbow("\n⇪ ").darkgoldenrod +  Rainbow("Invalid number. Please try again.").cyan
         index = gets.strip.to_i
       end  
 
